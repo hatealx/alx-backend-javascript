5ddd7b37de18ba02b2b2964bab6460ef255aab80
Abel <aatsou12@gmail.com>
2024-03-12 07:17:22 -0700

    done

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,16 +1,9 @@
 import { uploadPhoto, createUser } from './utils';
 
-// Prototype function handleProfileSignup
-function handleProfileSignup() {
-  // Collectively resolving promises returned by uploadPhoto and createUser
-  Promise.all([uploadPhoto(), createUser()])
-    .then(([userPhoto, user]) => {
-      // Logging body firstName lastName to the console
-      console.log(`${user.body.firstName} ${user.body.lastName}`);
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((values) => {
+      console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
     })
-    .catch(() => {
-      // Logging error message to the console in case of an error
-      console.error('Signup system offline');
-    });
+    .catch(() => console.log('Signup system offline'));
 }
-export default handleProfileSignup;
