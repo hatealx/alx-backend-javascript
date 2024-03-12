318ee8e621374a74b5e90c8d12d54e3b782b7343
Abel <aatsou12@gmail.com>
2024-03-12 06:32:11 -0700

    done

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,15 +1,12 @@
-function handleResponseFromAPI(promise) {
-  // Appending handlers to the promise
-  promise
-    .then(() => {
-      console.log('Got a response from the API'); // Log when the promise resolves
-      return { status: 200, body: 'success' }; // Return object when the promise resolves
-    })
-    .catch(() => {
-      console.log('Got a response from the API'); // Log when the promise rejects
-       const n = new Error()
-      return n; // Return empty Error object when the promise rejects
-    });
+export default function handleResponseFromAPI(promise) {
+  return promise.then(() => {
+    console.log('Got a response from the API');
+    return {
+      status: 200,
+      body: 'success',
+    };
+  }).catch(() => {
+    console.error();
+    return new Error();
+  });
 }
-
-export default handleResponseFromAPI;
