ce016e44b9097de15499a1953c1f6ae433860f26
Abel <aatsou12@gmail.com>
2024-03-20 06:18:59 -0700

    doen

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,6 +1,9 @@
 export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
+    if (typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
   }
 
   get sqft() {
