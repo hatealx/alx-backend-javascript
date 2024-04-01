f822a9f1be48691a557cf13d7e8110e7501d8c7b
Abel <aatsou12@gmail.com>
2024-04-01 08:16:33 -0700

    done

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,41 +1,41 @@
 export default class Car {
-    constructor(brand, motor, color) {
-        this._brand = brand;
-        this._motor = motor;
-        this._color = color;
-    }
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
 
-    cloneCar() {
-        const symbolKey = Symbol('Car');
-        const newCar = Object.assign(
-            Object.create(Object.getPrototypeOf(this)), 
-            this
-        );
-        Object.defineProperty(newCar, symbolKey, { value: true });
-        return newCar;
-    }
+  cloneCar() {
+    const symbolKey = Symbol('Car');
+    const newCar = Object.assign(
+      Object.create(Object.getPrototypeOf(this)),
+      this,
+    );
+    Object.defineProperty(newCar, symbolKey, { value: true });
+    return newCar;
+  }
 
-    get brand() {
-        return this._brand;
-    }
+  get brand() {
+    return this._brand;
+  }
 
-    set brand(value) {
-        this._brand = value;
-    }
+  set brand(value) {
+    this._brand = value;
+  }
 
-    get motor() {
-        return this._motor;
-    }
+  get motor() {
+    return this._motor;
+  }
 
-    set motor(value) {
-        this._motor = value;
-    }
+  set motor(value) {
+    this._motor = value;
+  }
 
-    get color() {
-        return this._color;
-    }
+  get color() {
+    return this._color;
+  }
 
-    set color(value) {
-        this._color = value;
-    }
+  set color(value) {
+    this._color = value;
+  }
 }
