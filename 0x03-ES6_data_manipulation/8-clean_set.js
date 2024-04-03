98384968b0f50ae7b0c70df836c24d115d86c6c8
Abel <aatsou12@gmail.com>
2024-04-03 18:21:38 +0000

    done

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,9 @@
+export default function cleanSet(set, startString) {
+  const result = [];
+  set.forEach((value) => {
+    if (value.startsWith(startString)) {
+      result.push(value.substring(startString.length));
+    }
+  });
+  return result.join('-');
+}
