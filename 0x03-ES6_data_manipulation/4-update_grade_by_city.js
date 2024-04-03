a4ae3d0d84aee6780331ecba245746e57f09781d
Abel <aatsou12@gmail.com>
2024-04-03 16:55:33 +0000

    done

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,11 @@
+export default function updateStudentGradeByCity(studentList, city, newGrades) {
+  return studentList
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const matchingGrade = newGrades.find((grade) => grade.studentId === student.id);
+      return {
+        ...student,
+        grade: matchingGrade ? matchingGrade.grade : 'N/A',
+      };
+    });
+}
